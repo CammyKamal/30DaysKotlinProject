ef974fe064f4e9bdfee4158947b80e6d1265de14
kamal <kamal.vaid@macys.com>
2020-06-07 18:49:31 +0530

    rename MainActivity.kt to CurrencyActivity.kt

--- app/src/main/java/org/kotlin/project/CurrencyActivity.kt
+++ app/src/main/java/org/kotlin/project/CurrencyActivity.kt
@@ -3,7 +3,7 @@ package org.kotlin.project
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CurrencyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
